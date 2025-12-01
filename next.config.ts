8e03cd2745638b3185e438c2ecad6d6613876bbb
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-12-01 14:25:07 +0700

    .

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  env: {
+    nextPublicApiUrl: 'http://localhost:5134/api/v1',
+  },
   typescript: {
     ignoreBuildErrors: false,
   },
